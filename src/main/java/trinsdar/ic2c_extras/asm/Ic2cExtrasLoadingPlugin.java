97d9b984ff64c32f9f39278254b8530ced526193
AyaSapphire <ayasapphire@qq.com>
2026-05-17 22:36:28 +0800

    use MixinBooter as the only mixin entrypoint

--- src/main/java/trinsdar/ic2c_extras/asm/Ic2cExtrasLoadingPlugin.java
+++ src/main/java/trinsdar/ic2c_extras/asm/Ic2cExtrasLoadingPlugin.java
@@ -1,55 +1,11 @@
 package trinsdar.ic2c_extras.asm;
 
-import net.minecraft.launchwrapper.LaunchClassLoader;
-import net.minecraftforge.common.ForgeVersion;
-import net.minecraftforge.fml.relauncher.CoreModManager;
-import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.jetbrains.annotations.Nullable;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
-import java.io.File;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-@IFMLLoadingPlugin.Name("Ic2cExtrasMixinLoader")
-@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
-@IFMLLoadingPlugin.TransformerExclusions("trinsdar.ic2c_extras.asm")
-@IFMLLoadingPlugin.SortingIndex(1001)
-public class Ic2cExtrasLoadingPlugin implements IFMLLoadingPlugin, ILateMixinLoader {
-    public Ic2cExtrasLoadingPlugin(){
-    }
-
-    @Override
-    public String[] getASMTransformerClass() {
-        return new String[0];
-    }
-
-    @Override
-    public String getModContainerClass() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getSetupClass() {
-        return null;
-    }
-
-    @Override
-    public void injectData(Map<String, Object> data) {
-
-    }
-
-    private void loadModJar(File jar) throws Exception {
-        ((LaunchClassLoader) this.getClass().getClassLoader()).addURL(jar.toURI().toURL());
-        CoreModManager.getReparseableCoremods().add(jar.getName());
-    }
-
-    @Override
-    public String getAccessTransformerClass() {
-        return null;
-    }
+public class Ic2cExtrasLoadingPlugin implements ILateMixinLoader {
 
     @Override
     public List<String> getMixinConfigs() {
